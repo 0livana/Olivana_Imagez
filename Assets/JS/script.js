6e5ffbc2ab416aa8cbbfc7a59604226bff368ca0
Olivana <110850743+0livana@users.noreply.github.com>
2022-12-22 17:29:19 +0100

    Add files via upload

---
+++ Assets/JS/script.js
@@ -0,0 +1,10 @@
+var prices=document.querySelector("#price")
+
+function openPrices(){
+    prices.style.display="block"
+    document.querySelector("nav").style.opacity="0.2"
+}
+
+function showPort(){
+    document.querySelector("#pdropdown").style.display="block"
+}
